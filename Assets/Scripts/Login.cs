27732f072b2369abca79c37b20455ee04e6eee2c
sandhya <sandhya.nethra96@gmail.com>
2020-10-17 17:20:25 +0530

    Login Scene
    Login scene

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Login : MonoBehaviour
+{
+    public InputField userName;
+    public GameObject validInput;
+    public void OnClickLogin()
+    {
+        string name = userName.text;
+        if(!string.IsNullOrEmpty(name))
+        {
+            SceneManager.LoadScene("AppScene");
+        }
+        else
+        {
+            Debug.Log("Please enter the name");
+            validInput.SetActive(true);
+        }
+    }
+
+}
